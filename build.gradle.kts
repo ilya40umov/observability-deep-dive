d650de816cac5b493b09e0eff08f340ee6453ce1
Illia Sorokoumov <illia.sorokoumov@bettermile.com>
2024-05-02 10:45:43 +0200

    Bump up Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "3.2.5" apply false
     id("io.spring.dependency-management") version "1.1.4" apply false
-    kotlin("jvm") version "1.9.22" apply false
-    kotlin("plugin.spring") version "1.9.22" apply false
+    val kotlinVersion = "1.9.23"
+    kotlin("jvm") version kotlinVersion apply false
+    kotlin("plugin.spring") version kotlinVersion apply false
 }
 
 allprojects {
